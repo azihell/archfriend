bf8fdb0dfd0eec775f618b7a7ce30e208ca117af
Aziel Freitas <azielfreitas@gmail.com>
2021-12-25 12:32:22 -0300

    Add long list of packages to install with pacman

---
+++ pacfiles.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo pacman -S --needed networkmanager network-manager-applet wireless_tools wpa_supplicant base-devel linux-headers git bluez bluez-utils pulseaudio-bluetooth cups openssh bash-completion firewalld rsync pavucontrol python python-pip unzip dhcpcd exa neofetch xfce4-screenshooter fish firefox archlinux-wallpaper alsa-utils zathura zathura-pdf-poppler xf86-video-intel xorg lightdm lightdm-gtk-greeter qtile nitrogen kitty brightnessctl
